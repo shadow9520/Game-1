b3b1335ba3b6a3ede664240a28b7570a0dc40b02
shadowfang95 <>
2021-05-05 19:21:50 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+The game you are about to see is a fun one so heres the warning.
+- ***you are about to Have fun!!!!!***
+
+did you finish the thing because it's not working like its suppose to .
---
+++ scene1.js
@@ -0,0 +1,10 @@
+var Sceneone = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize: function() {
+      Phaser.Scene.call(this, { "key": "Sceneone" });
+    },
+    init: function() {},
+    preload: function() {},
+    create: function() {},
+    update: function() {}
+});
